1669f8e604e0f3a68ef808fc11c5076b553a2d21
Lee Kwenwon <highfence@naver.com>
2017-06-27 22:45:51 +0900

    [Refactoring] 리팩토링 시작

---
+++ MapEditer/ProgramMain.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE prevhInstance, LPSTR lpszCmdParam, int nCmdParam)
+{
+	WNDCLASS WndClass;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+
+
+}
