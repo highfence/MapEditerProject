cf9dc9ee32894bda35e68fbe11a44d9cb1ca3e80
Lee Kwenwon <highfence@naver.com>
2017-06-30 11:33:33 +0900

    [Option Window] Make Go to Origin button

--- MapEditer/OptionWindow.cpp
+++ MapEditer/OptionWindow.cpp
@@ -41,14 +41,17 @@ namespace DXMapEditer
 
 	void OptionWindow::CreateWindows(HWND hWnd)
 	{
-		CreateWindow(L"button", L"Camera", WS_CHILD | WS_VISIBLE | BS_GROUPBOX,
+		CreateWindow(TEXT("button"), TEXT("Camera"), WS_CHILD | WS_VISIBLE | BS_GROUPBOX,
 			5, 0, 275, 110, hWnd, (HMENU)0, _hInst, NULL);
 
-		CreateWindow(L"static", L"Move Speed", WS_CHILD | WS_VISIBLE,
+		CreateWindow(TEXT("static"), TEXT("Move Speed"), WS_CHILD | WS_VISIBLE,
 			20, 20, 100, 25, hWnd, (HMENU)-1, _hInst, NULL);
 
 		_MoveSpeedEdit = CreateWindow(TEXT("edit"), TEXT("300"), WS_CHILD | WS_VISIBLE | WS_BORDER,
 			125, 20, 50, 25, hWnd, (HMENU)1, _hInst, NULL);
+
+		CreateWindow(TEXT("button"), TEXT("Go To Origin"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			20, 50, 155, 25, hWnd, (HMENU)2, _hInst, NULL);
 	}
 
 	LRESULT OptionWindowProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
@@ -57,7 +60,6 @@ namespace DXMapEditer
 		{
 		case WM_CREATE :
 		{
-			// TODO 여기서부터
 			optionWindowHandle->CreateWindows(hWnd);
 			break;
 		}
